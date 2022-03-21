39c6e21c3a38716e2f1e247a5beb79d2b974d594
Pushpam Kumar <kumarpushpam99@gmail.com>
2022-03-21 21:58:24 +0530

    added comments in user service

--- src/main/java/com/example/tiny_url/service/UserService.java
+++ src/main/java/com/example/tiny_url/service/UserService.java
@@ -5,6 +5,11 @@ import com.example.tiny_url.repository.UserRepository;
 import org.springframework.stereotype.Service;
 import reactor.core.publisher.Mono;
 
+/**
+ * <h1>UserService</h1>
+ * @author Pushpam Kumar
+ * A Service which deals user database eg. creating user, getting user etc etc.
+ */
 @Service
 public class UserService {
     private final UserRepository userRepository;
@@ -13,11 +18,21 @@ public class UserService {
         this.userRepository = userRepository;
     }
 
+    /**
+     * A method which is used to register new user.
+     * @param name The name of the new user
+     * @return Mono<User> Returns the details of created user.
+     */
     public Mono<User> registerUser(String name) {
         Mono<User> save = userRepository.save(new User(name));
         return save;
     }
 
+    /**
+     * A method to get the user details by id
+     * @param id id of the user to search
+     * @return Mono<User> Returns the details of the user
+     */
     public Mono<User> getUser(String id) {
         return userRepository.findById(id);
     }
